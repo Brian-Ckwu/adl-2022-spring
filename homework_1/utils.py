b40e2193437a8844fbcdc7f445eeb242c5cc7b1a
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-18 19:12:57 +0800

    implement same_seeds() & save_checkpoint()

--- homework_1/utils.py
+++ homework_1/utils.py
@@ -1,5 +1,11 @@
-from typing import Iterable, List
-
+import json
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+import pathlib
+from argparse import Namespace
+from typing import Callable, Iterable, List, Dict
 
 class Vocab:
     PAD = "[PAD]"
@@ -42,3 +48,34 @@ class Vocab:
 def pad_to_len(seqs: List[List[int]], to_len: int, padding: int) -> List[List[int]]:
     paddeds = [seq[:to_len] + [padding] * max(0, to_len - len(seq)) for seq in seqs]
     return paddeds
+
+def same_seeds(seed: int = 7):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def save_checkpoint(args: Namespace, model: nn.Module, objective_metric: float):
+    ckpt_name = f"hidden-{args.hidden_size}_nlayers-{args.num_layers}_dropout-{args.dropout}_lr-{args.lr}_batch-{args.batch_size}"
+    ckpt_dir: pathlib.PosixPath = args.model_dir / ckpt_name
+    ckpt_dir.mkdir(parents=True, exist_ok=True)
+    # save config
+    args_d = dict()
+    for key, value in vars(args).items():
+        if type(value) != pathlib.PosixPath:
+            args_d[key] = value
+    with open(ckpt_dir / "config.json", mode="wt") as f:
+        json.dump(args_d, f)
+
+    # save model
+    torch.save(model.state_dict(), ckpt_dir / "model.pth")
+
+    # save evaluation results
+    with open(ckpt_dir / "metrics.txt", mode="wt") as f:
+        f.write(str(objective_metric))
+
+
+def grid_search(trainer: Callable, args: Namespace, target_hparams: Dict[str, List]):
+    return
