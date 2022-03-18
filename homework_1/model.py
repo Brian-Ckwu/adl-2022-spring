549e46fd38bca9e8373dcd9aaffc8115ab29b644
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-18 14:11:54 +0800

    implemented SeqClassifier

--- homework_1/model.py
+++ homework_1/model.py
@@ -1,10 +1,11 @@
 from typing import Dict
 
 import torch
-from torch.nn import Embedding
+import torch.nn as nn
+from torch.nn import Embedding, LSTM
 
 
-class SeqClassifier(torch.nn.Module):
+class SeqClassifier(nn.Module):
     def __init__(
         self,
         embeddings: torch.tensor,
@@ -15,14 +16,37 @@ class SeqClassifier(torch.nn.Module):
         num_class: int,
     ) -> None:
         super(SeqClassifier, self).__init__()
+        self.hidden_size = hidden_size
         self.embed = Embedding.from_pretrained(embeddings, freeze=False)
+        embed_dim = self.embed.weight.shape[1]
         # TODO: model architecture
+        D = 2 if bidirectional else 1
+        self.lstm = LSTM(input_size=embed_dim, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, dropout=dropout, bidirectional=bidirectional)
+        self.mlp = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(D * hidden_size, D * hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(D * hidden_size, num_class)
+        )
 
     @property
     def encoder_output_size(self) -> int:
         # TODO: calculate the output dimension of rnn
         raise NotImplementedError
 
+    # reference method: https://towardsdatascience.com/lstm-text-classification-using-pytorch-2c6c657f8fc0
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
-        raise NotImplementedError
+        text_len = batch.shape[1]
+        # embedding layer
+        text_emb = self.embed(batch) # -> (B, L, D * H)
+        # LSTM layer
+        output, _ = self.lstm(text_emb)
+        out_forward = output[:, text_len - 1, :self.hidden_size]
+        out_reverse = output[:, 0, self.hidden_size:]
+        out_concat = torch.cat(tensors=(out_forward, out_reverse), dim=1) # -> (B, D * H)
+        # MLP classification layer
+        class_scores = self.mlp(out_concat)
+
+        return class_scores
