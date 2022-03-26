f5c54a99995d0554782425c92549bcfc1a5f5620
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-26 18:30:49 +0800

    implement BertSlotTagger

--- homework_1_bert/model_bert.py
+++ homework_1_bert/model_bert.py
@@ -10,7 +10,7 @@ class BertIntentClassifier(nn.Module):
     def __init__(self, args: Namespace, num_classes: int) -> None:
         super(BertIntentClassifier, self).__init__()
         # model
-        self.bert = BertModel.from_pretrained(args.bert_type)
+        self.bert = BertModel.from_pretrained(args.bertType)
         self.fc = nn.Linear(
             in_features=self.bert.embeddings.word_embeddings.embedding_dim,
             out_features=num_classes
@@ -26,3 +26,21 @@ class BertIntentClassifier(nn.Module):
 
     def calc_loss(self, y_scores, y_true):
         return self.criterion(y_scores, y_true)
+
+class BertSlotTagger(nn.Module):
+    def __init__(self, args: Namespace, num_classes: int) -> None:
+        super(BertSlotTagger, self).__init__()
+        # model
+        self.bert = BertModel.from_pretrained(args.bertType)
+        self.fc = nn.Linear(
+            in_features=self.bert.embeddings.word_embeddings.embedding_dim,
+            out_features=num_classes
+        )
+        # loss
+        self.criterion = nn.CrossEntropyLoss(reduction="mean")
+    
+    def forward(self, x: BatchEncoding) -> torch.FloatTensor:
+        h = self.bert(**x).last_hidden_state
+        h_slots = h[:, 1:-1, :]
+        scores = self.fc(h_slots)
+        return scores
