59ce404451b9158658a878c4d748dc95aae5b332
Brian-Ckwu <sonny40920710@gmail.com>
2022-03-26 14:26:16 +0800

    modify train_intent.py to suit BERT implementation

--- homework_1_bert/train_intent.py
+++ homework_1_bert/train_intent.py
@@ -1,18 +1,19 @@
 import time
 import json
 import pickle
-from genericpath import samestat
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import Dict, List
 
 import torch
 from tqdm import trange
+from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
+from transformers import BertTokenizerFast
 
-from dataset import SeqClsDataset
+from dataset import IntentClsDataset
 from utils import *
-from model import SeqClassifier
+from model_bert import BertIntentClassifier
 
 TRAIN = "train"
 DEV = "eval"
@@ -22,53 +23,40 @@ def trainer(args) -> float: # return the objective metric
     start_time = time.time()
     # handle directories
     args.model_dir.mkdir(parents=True, exist_ok=True)
-    args.ckpt_dir = args.model_dir / render_model_name(args, hparams=[
-        "rnn_type", 
-        "hidden_size", 
-        "num_layers", 
-        "rnn_dropout",
-        "mlp_dropout", 
-        "lr", 
-        "batch_size", 
-        "optimizer",
-        "scheduler"]
+    args.ckpt_dir = args.model_dir / render_model_name(args, hparams=[ 
+            "bertType",
+            "lr", 
+            "wd",
+            "bs", 
+            "optimizer"
+        ]
     )
     args.ckpt_dir.mkdir(parents=True, exist_ok=True)
 
     same_seeds(args.seed)
     # Data
     print(f"Loading data...")
-    with open(args.cache_dir / "vocab.pkl", "rb") as f:
-        vocab: Vocab = pickle.load(f)
+    tokenizer = BertTokenizerFast.from_pretrained(args.bertType)
 
     intent_idx_path = args.cache_dir / "intent2idx.json"
     intent2idx: Dict[str, int] = json.loads(intent_idx_path.read_text())
 
     data_paths = {split: args.data_dir / f"{split}.json" for split in SPLITS}
     data = {split: json.loads(path.read_text()) for split, path in data_paths.items()}
-    datasets: Dict[str, SeqClsDataset] = {
-        split: SeqClsDataset(split_data, vocab, intent2idx, args.max_len)
+    datasets: Dict[str, IntentClsDataset] = {
+        split: IntentClsDataset(data=split_data, tokenizer=tokenizer, label_mapping=intent2idx)
         for split, split_data in data.items()
     }
 
-    train_loader = DataLoader(datasets[TRAIN], args.batch_size, shuffle=True, pin_memory=True, collate_fn=datasets[TRAIN].collate_fn)
-    val_loader = DataLoader(datasets[DEV], args.batch_size, shuffle=False, pin_memory=True, collate_fn=datasets[DEV].collate_fn)
+    train_loader = DataLoader(datasets[TRAIN], batch_size=args.bs, shuffle=True, pin_memory=True, collate_fn=datasets[TRAIN].collate_fn)
+    val_loader = DataLoader(datasets[DEV], batch_size=args.bs, shuffle=False, pin_memory=True, collate_fn=datasets[DEV].collate_fn)
 
     # Model
     print(f"Init model & optimizer...")
     embeddings = torch.load(args.cache_dir / "embeddings.pt")
-    model = SeqClassifier(
-        embeddings=embeddings, 
-        rnn_type=args.rnn_type, 
-        hidden_size=args.hidden_size, 
-        num_layers=args.num_layers, 
-        rnn_dropout=args.rnn_dropout, 
-        mlp_dropout=args.mlp_dropout,
-        bidirectional=args.bidirectional, 
-        num_class=len(intent2idx)
-    ).to(args.device)
-    optimizer = getattr(torch.optim, args.optimizer)(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    scheduler = getattr(torch.optim.lr_scheduler, args.scheduler)(optimizer, T_max=args.num_epoch, verbose=True)
+    model = BertIntentClassifier(args, num_classes=datasets[TRAIN].num_classes).to(args.device)
+    optimizer = getattr(torch.optim, args.optimizer)(model.parameters(), lr=args.lr, weight_decay=args.wd)
+    # scheduler = getattr(torch.optim.lr_scheduler, args.scheduler)(optimizer, T_max=args.num_epoch, verbose=True)
 
     # Optimization
     print(f"\n ===== Start training ===== \n")
@@ -78,12 +66,12 @@ def trainer(args) -> float: # return the objective metric
         "val_acc": list(),
         "val_loss": list(),
     }
-    no_improve_epochs = 0
+    stale = 0
     best_val_acc = 0
     for epoch in trange(args.num_epoch, desc="Epoch"):
         model.train()
-        for x, y in train_loader:
-            x, y = x.to(args.device), y.to(args.device)
+        for x, y, _ in tqdm(train_loader):
+            x, y = move_dict_to_device(x, args.device), y.to(args.device)
             # make prediction & calculate loss
             scores = model(x)
             loss = model.calc_loss(scores, y)
@@ -97,7 +85,7 @@ def trainer(args) -> float: # return the objective metric
         train_acc, train_loss = evaluate_model(model, train_loader, args.device) if args.record_train else (0, 0)
         val_acc, val_loss = evaluate_model(model, val_loader, args.device)
 
-        scheduler.step()
+        # scheduler.step()
 
         for key, value in zip(["train_acc", "train_loss", "val_acc", "val_loss"], [train_acc, train_loss, val_acc, val_loss]):
             train_log[key].append(value)
@@ -105,13 +93,13 @@ def trainer(args) -> float: # return the objective metric
         if val_acc > best_val_acc:
             # save config, model, and evaluation result
             best_val_acc = val_acc
-            no_improve_epochs = 0
+            stale = 0
             args.best_epoch = epoch + 1
             save_checkpoint(args, model, best_val_acc)
         else:
-            no_improve_epochs += 1
-            print(f"No improvement for {no_improve_epochs} epochs.")
-            if no_improve_epochs > args.es_epoch:
+            stale += 1
+            print(f"No improvement for {stale} epochs.")
+            if stale > args.es_epoch:
                 print(f"Training halted at epoch {epoch + 1}.")
                 break
 
@@ -125,12 +113,12 @@ def trainer(args) -> float: # return the objective metric
 
     return best_val_acc, train_log
 
-def evaluate_model(model: SeqClassifier, loader: DataLoader, device: str) -> tuple:
+def evaluate_model(model: nn.Module, loader: DataLoader, device: str) -> tuple:
     model.eval()
     correct, samples = 0, 0 # acc
     total_loss = 0 # loss
-    for x, y in loader:
-        x, y = x.to(device), y.to(device)
+    for x, y, _ in loader:
+        x, y = move_dict_to_device(x, args.device), y.to(device)
         with torch.no_grad():
             scores = model(x)
             # acc
@@ -168,32 +156,24 @@ def parse_args() -> Namespace:
     parser.add_argument("--seed", type=int, default=7)
 
     # data
-    parser.add_argument("--max_len", type=int, default=128)
+    parser.add_argument("--bs", type=int, default=32)
 
     # model
-    parser.add_argument("--rnn_type", type=str, default="LSTM")
-    parser.add_argument("--hidden_size", type=int, default=256)
-    parser.add_argument("--num_layers", type=int, default=1)
-    parser.add_argument("--rnn_dropout", type=float, default=0.50)
-    parser.add_argument("--mlp_dropout", type=float, default=0.50)
-    parser.add_argument("--bidirectional", type=bool, default=True)
+    parser.add_argument("--bertType", type=str, default="bert-base-uncased")
 
     # optimizer
-    parser.add_argument("--optimizer", type=str, default="NAdam")
-    parser.add_argument("--lr", type=float, default=2e-3)
-    parser.add_argument("--weight_decay", type=float, default=0)
+    parser.add_argument("--optimizer", type=str, default="Adam")
+    parser.add_argument("--lr", type=float, default=2e-5)
+    parser.add_argument("--wd", type=float, default=0)
 
     # schedular
-    parser.add_argument("--scheduler", type=str, default="CosineAnnealingLR")
-
-    # data loader
-    parser.add_argument("--batch_size", type=int, default=32)
+    # parser.add_argument("--scheduler", type=str, default="CosineAnnealingLR")
 
     # training
     parser.add_argument(
         "--device", type=str, help="cpu, cuda, cuda:0, cuda:1", default="cuda:0"
     )
-    parser.add_argument("--num_epoch", type=int, default=50)
+    parser.add_argument("--num_epoch", type=int, default=10)
     parser.add_argument("--es_epoch", type=int, default=10)
     parser.add_argument("--record_train", type=bool, default=True)
 
@@ -211,4 +191,10 @@ def render_model_name(args: Namespace, hparams: List[str]) -> str:
 
 if __name__ == "__main__":
     args = parse_args()
-    trainer(args)
+    target_lr = [3e-5, 5e-5]
+    target_bs = [16, 32]
+    for bs in target_bs:
+        for lr in target_lr:
+            args.bs = bs
+            args.lr = lr
+            trainer(args)
